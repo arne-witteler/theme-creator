c0864a1bd0e34f1bd22747be2972d830ee0455af
Arne Witteler <witteler@arnenoah.de>
2024-04-05 10:06:18 +0200

    add solution for task 3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import "./App.css";
 import Theme from "./components/Theme/Theme";
-import { themes } from "./db";
+import ThemeForm from "./components/ThemeForm/ThemeForm";
+import { themes as initialThemes } from "./db";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 function App() {
+  const [themes, setThemes] = useState(initialThemes);
+
+  function handleAddTheme(newTheme) {
+    const newThemeWithId = { ...newTheme, id: uuid() };
+    setThemes([newThemeWithId, ...themes]);
+  }
+
   return (
     <>
       <header className="header">
         <h1>Theme Creator</h1>
       </header>
-      <h2>Add a theme</h2>
-      <form id="themeForm">
-        <label for="themeTitle">Theme Title:</label>
-        <input type="text" id="themeTitle" name="themeTitle" required />
-
-        <label for="color1">Color 1:</label>
-        <input type="color" id="color1" name="color1" required />
-
-        <label for="color2">Color 2:</label>
-        <input type="color" id="color2" name="color2" required />
-
-        <label for="color3">Color 3:</label>
-        <input type="color" id="color3" name="color3" required />
-
-        <label for="color4">Color 4:</label>
-        <input type="color" id="color4" name="color4" required />
-
-        <button type="submit">Add Theme</button>
-      </form>
       <main className="main-container">
+        <ThemeForm onSubmit={handleAddTheme} />
         <ul className="theme-list">
           {themes.map((theme) => (
             <li key={theme.id}>
---
+++ src/components/ThemeForm/ThemeForm.js
@@ -0,0 +1,76 @@
+import "./ThemeForm.css";
+
+const intialTheme = {
+  name: "",
+  colors: [
+    { role: "primary", value: "#ffffff" },
+    { role: "secondary", value: "#ffffff" },
+    { role: "surface", value: "#ffffff" },
+    { role: "surface-on", value: "#ffffff" },
+  ],
+};
+
+export default function ThemeForm({ onSubmit }) {
+  const initialData = intialTheme;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    const newTheme = {
+      name: data.name,
+      colors: [
+        {
+          role: "primary",
+          value: data.primary,
+        },
+        {
+          role: "secondary",
+          value: data.secondary,
+        },
+        {
+          role: "surface",
+          value: data.surface,
+        },
+        {
+          role: "surface-on",
+          value: data["surface-on"],
+        },
+      ],
+    };
+
+    onSubmit(newTheme);
+  }
+
+  return (
+    <form className="theme-form" onSubmit={handleSubmit}>
+      <h2 className="theme-form__title">Add a theme</h2>
+      <input
+        aria-label="theme title"
+        className="theme-form__name-input"
+        type="text"
+        placeholder="Theme Name"
+        name="name"
+        defaultValue={initialData.name}
+        required
+      />
+      <fieldset className="theme-form__color-inputs">
+        {initialData.colors.map((color) => (
+          <input
+            key={color.role}
+            className="theme-form__color-input"
+            type="color"
+            name={color.role}
+            aria-label={color.role}
+            defaultValue={color.value}
+          />
+        ))}
+      </fieldset>
+      <button className="theme-form__submit-button" type="submit">
+        Add theme
+      </button>
+    </form>
+  );
+}
